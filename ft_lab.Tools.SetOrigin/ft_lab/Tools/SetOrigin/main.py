73e06737585f4aadf539dccdb87fc358a5a9685b
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2022-04-23 00:13:01 +0900

    メニューを追加

--- ft_lab.Tools.SetOrigin/ft_lab/Tools/SetOrigin/main.py
+++ ft_lab.Tools.SetOrigin/ft_lab/Tools/SetOrigin/main.py
@@ -1,15 +1,65 @@
 from pxr import Usd, UsdGeom, UsdSkel, UsdPhysics, UsdShade, UsdSkel, Sdf, Gf, Tf
 import omni.ext
-import carb.events
 import omni.usd
+import omni.kit.menu.utils
+import omni.kit.undo
+import omni.kit.commands
+import omni.usd
+from omni.kit.menu.utils import MenuItemDescription
+import asyncio
 
 # ----------------------------------------------------.
 class SetOriginExtension (omni.ext.IExt):
+    # Menu list.
+    _menu_list = None
+    _sub_menu_list = None
+
+    # Menu name.
+    _menu_name = "Tools"
+
     # ------------------------------------------.
-    # Update event.
+    # Initialize menu.
     # ------------------------------------------.
-    def on_update (self, e: carb.events.IEvent):
-        pass
+    def init_menu (self):
+        async def _rebuild_menus():
+            await omni.kit.app.get_app().next_update_async()
+            omni.kit.menu.utils.rebuild_menus()
+
+        def menu_select (mode):
+            if mode == 0:
+                print("Select MenuItem 1.")
+            if mode == 1:
+                print("Select MenuItem 2.")
+            if mode == 2:
+                print("Select MenuItem 3.")
+
+        self._sub_menu_list = [
+            MenuItemDescription(name="Center of Geometry", onclick_fn=lambda: menu_select(0)),
+            MenuItemDescription(name="Pivot Center of Geometry", onclick_fn=lambda: menu_select(1)),
+        ]
+
+        self._menu_list = [
+            MenuItemDescription(name="Set Origin", sub_menu=self._sub_menu_list),
+        ]
+
+        # Rebuild with additional menu items.
+        omni.kit.menu.utils.add_menu_items(self._menu_list, self._menu_name)
+        asyncio.ensure_future(_rebuild_menus())
+
+    # ------------------------------------------.
+    # Term menu.
+    # It seems that the additional items in the top menu will not be removed.
+    # ------------------------------------------.
+    def term_menu (self):
+        async def _rebuild_menus():
+            await omni.kit.app.get_app().next_update_async()
+            omni.kit.menu.utils.rebuild_menus()
+
+        # Remove and rebuild the added menu items.
+        omni.kit.menu.utils.remove_menu_items(self._menu_list, self._menu_name)
+        asyncio.ensure_future(_rebuild_menus())
+
+    # ------------------------------------------.
 
     # ------------------------------------------.
     # Extension startup.
@@ -17,10 +67,15 @@ class SetOriginExtension (omni.ext.IExt):
     def on_startup (self, ext_id):
         print("[ft_lab.Tools.SetOrigin] startup")
 
+        # Initialize menu.
+        self.init_menu()
+
     # ------------------------------------------.
     # Extension shutdown.
     # ------------------------------------------.
     def on_shutdown(self):
         print("[ft_lab.Tools.SetOrigin] shutdown")
 
+        # Term menu.
+        self.term_menu()
 
